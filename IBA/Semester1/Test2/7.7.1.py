c94dbd1ce041dd694f1ea20cb88ad090e49e93d0
AllanFrank2023 <allan.frank4@icloud.com>
2023-09-14 16:03:52 +0200

    13

--- IBA/Semester1/Test2/7.7.1.py
+++ IBA/Semester1/Test2/7.7.1.py
@@ -7,11 +7,13 @@
 print("Velkommen til dit ugen time-beregner")
 
 timer = float(input("Indtast timeantal :  "))
+timeLøn = float(input("Indtast din timeløn: "))
 
 if timer > 40.0:
     overtid = timer - 40
-    overtidsløn = (overtid * (1.5 * 300))
-    print("Din overtid er:", overtid, " udbetaling af overtid i denne uge er (timeløn 300kr) ", overtidsløn)
+    overtidsløn = (overtid * (1.5 * timeLøn))
+    løn = timeLøn * 40
+    print("Din overtid er:", overtid, " udbetaling af overtid i denne uge er (timeløn 300kr) ", overtidsløn, "din alm løn: ", løn, "I alt:", (løn + overtidsløn))
 else: 
         print("Du har ingen overtid i denne her uge")
 
