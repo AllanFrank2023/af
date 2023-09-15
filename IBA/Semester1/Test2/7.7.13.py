9f4dac2a1a2f3b2ab92cd11b854d0a12ce060090
AllanFrank2023 <allan.frank4@icloud.com>
2023-09-15 17:54:11 +0200

    1

--- IBA/Semester1/Test2/7.7.13.py
+++ IBA/Semester1/Test2/7.7.13.py
@@ -8,3 +8,58 @@
 
 #Dato verificering
 
+date_input = input("Indtast måned/dag/år (ex 5/15/2023): ")
+# Splitter input op i måned, dag og år
+month, day, year = map(int, date_input.split('/'))
+
+# Dage antal over 28 (ikke skudår)
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] #[3, 0, 3, 2, 3, 2, 3, 3, 2, 3, 2, 3]
+
+#Skudår
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) and month > 2:
+    skuddag = 1
+    print(year, "er skudår.")
+    
+else:
+    print( year, "er ikke skudår.")
+    skuddag = 0
+
+# Valider input
+if month < 1 or month > 12 or day < 1 or day > days_in_month[month - 1] or year < 1:
+    print("Ikke gyldig format -  Prøv venligst igen.")
+else:
+
+
+     # Beregner dagen
+    
+    day_num = (28 * (month -1)) + day 
+    day_num = day_num + skuddag
+    
+    #Beregner Opgave B
+if month > 3:
+    day_num2 = day_num - ((4 * (month) + 23)//10)
+else:
+        print("Opgave B : ", day_num)
+    # Display the result
+print("#########################")
+print("Spørgsmål A & C : Dagen er nummer:", day_num)
+print("Skudag er lagt til: ", skuddag)
+print("Opgave B løsning:", day_num2)
+   
+#############################################
+
+
+# Er det skudår ??
+# Input af årstal
+###year = int(input("Indtast årstal: "))
+
+###if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+###    print(f"{year} er skudår.")
+###else:  print(f"{year} er ikke skudår.")
+
+    # Skudår eller ej
+#if is_leap_year(year):
+       
+#else:
+       
+        
