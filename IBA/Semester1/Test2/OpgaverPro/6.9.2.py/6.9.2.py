79deb70594db49a9ca00e4045cb303448d350887
AllanFrank2023 <allan.frank4@icloud.com>
2023-09-21 19:16:33 +0200

    12

--- IBA/Semester1/Test2/OpgaverPro/6.9.2.py/6.9.2.py
+++ IBA/Semester1/Test2/OpgaverPro/6.9.2.py/6.9.2.py
@@ -14,3 +14,42 @@
 
 
 
+def sangen():
+    song()
+    songP2()
+    song()
+    songP2() 
+
+    
+def song():
+    print("The ants go marching one by one, hurrah! hurrah")
+    print("The ants go marching one by one, hurrah! hurrah")
+    print("The ants go marching one by one, hurrah! hurrah")
+    print("The ants go marching one by one.")
+
+def songP2():
+    print("The little one stops to suck his thumb,")
+    print("And they all go marching down...")
+    print("In the ground...")
+    print("To get out....")
+    print("Of the rain.")
+    print("Boom! Boom! Boom!")
+
+def main():
+ 
+# Input the number of songs showed from the user
+    n = int(input("Indtast det antal gange sangen skal ses: "))
+
+# Print the requested number of songs with Functions
+    for i in range(1, n + 1):
+        sangen()
+        print("Nummer: ", i)
+        print(" ")
+main()
+       
+
+        
+    ######Færdig    
+
+
+    
--- IBA/Semester1/Test2/OpgaverPro/6.9.2.py/LommeregnerModul.py
+++ IBA/Semester1/Test2/OpgaverPro/6.9.2.py/LommeregnerModul.py
@@ -1,1 +1,6 @@
 #Konverter lommeregner til modul - skriv program der kalder lommeregneren og giver det nødvendige input
+
+
+
+
+def 
